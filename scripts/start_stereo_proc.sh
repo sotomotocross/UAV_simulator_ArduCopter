4116d072679b859f16eb731830552aceb16ded64
Sotirios Aspragkathos <s.aspragkathos@gmail.com>
2020-11-27 19:51:25 +0200

    Update start_stereo_proc.sh

--- scripts/start_stereo_proc.sh
+++ scripts/start_stereo_proc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ~/ROS_workspaces/ardupilot_ws/
+cd ~/csl_uav_simulator_ws/
 source devel/setup.bash
 ROS_NAMESPACE=iris_demo/ZED_stereocamera/camera rosrun stereo_image_proc stereo_image_proc _approximate_sync:=True
