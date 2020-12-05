acd4115626e03ef0e5b36fa12ee296269bb93f26
Sotirios Aspragkathos <s.aspragkathos@gmail.com>
2020-12-05 10:43:10 +0200

    Update startsitl.sh

--- scripts/startsitl.sh
+++ scripts/startsitl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/ardupilot/ArduCopter/
-sim_vehicle.py --mavproxy-args="--streamrate=140" -v ArduCopter -f gazebo-iris --console --map
+sim_vehicle.py --mavproxy-args="--streamrate=100" -v ArduCopter -f gazebo-iris --console --map
